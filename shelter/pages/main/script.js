67d37e7c1e2c0b26efc4197bbe948f4cc554e979
Anna Rogozina <annettgur@gmail.com>
2022-04-14 14:36:07 +0300

    feat: add responsibility for main768

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,1 +1,20 @@
 console.log("в работе есть нестыковки с макетом в навигации, но не более 5 пикселей. Все требования тз выполнены. Оценка:100");
+const hamburger = document.querySelector('.hamburger');
+const headerNavigation = document.querySelector('.header__navigation');
+const navigationNonbordered = document.querySelector('.navigation_nonbordered');
+function toggleMenu() {
+  hamburger.classList.toggle('open');
+  headerNavigation.classList.toggle('active');
+  navigationNonbordered.classList.toggle('active');
+}
+hamburger.addEventListener('click', toggleMenu);
+
+function closeMenu() {
+     
+        hamburger.classList.remove('open');
+        headerNavigation.classList.remove('active');
+        link.classList.remove('active');
+       
+    }
+  
+  headerNavigation.addEventListener('click', closeMenu);
