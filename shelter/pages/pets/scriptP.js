d373ab0b9465ecdbd2590116d2a6860007ec1f12
Anna Rogozina <annettgur@gmail.com>
2022-04-16 00:07:00 +0300

    feat: finish adoptive

--- shelter/pages/pets/scriptP.js
+++ shelter/pages/pets/scriptP.js
@@ -1,1 +1,20 @@
 console.log("100");
+const hamburger = document.querySelector('.hamburger');
+const headerNavigation = document.querySelector('.header__navigation');
+const navigationNonbordered = document.querySelector('.navigation_nonbordered');
+function toggleMenu() {
+  hamburger.classList.toggle('open');
+  headerNavigation.classList.toggle('active');
+  navigationNonbordered.classList.toggle('active');
+}
+hamburger.addEventListener('click', toggleMenu);
+
+function closeMenu() {
+     
+        hamburger.classList.remove('open');
+        headerNavigation.classList.remove('active');
+        link.classList.remove('active');
+       
+    }
+  
+  headerNavigation.addEventListener('click', closeMenu);
