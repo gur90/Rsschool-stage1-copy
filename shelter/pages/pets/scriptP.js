dc313cb51dce2baafe04401c8e379444d8f77382
Anna Rogozina <annettgur@gmail.com>
2022-04-16 14:31:10 +0300

    fix: fix responsibility on 319px

--- shelter/pages/pets/scriptP.js
+++ shelter/pages/pets/scriptP.js
@@ -1,10 +1,10 @@
 console.log("100");
 const hamburger = document.querySelector('.hamburger');
-const headerNavigation = document.querySelector('.header__navigation');
+const navigation = document.querySelector('.header__navigation');
 const navigationNonbordered = document.querySelector('.navigation_nonbordered');
 function toggleMenu() {
   hamburger.classList.toggle('open');
-  headerNavigation.classList.toggle('active');
+  navigation.classList.toggle('active');
   navigationNonbordered.classList.toggle('active');
 }
 hamburger.addEventListener('click', toggleMenu);
@@ -12,9 +12,9 @@ hamburger.addEventListener('click', toggleMenu);
 function closeMenu() {
      
         hamburger.classList.remove('open');
-        headerNavigation.classList.remove('active');
-        link.classList.remove('active');
+        navigation.classList.remove('active');
+        navigationNonbordered.classList.remove('active');
        
     }
   
-  headerNavigation.addEventListener('click', closeMenu);
+  navigation.addEventListener('click', closeMenu);
