3b845ac25f028f2a706eacfcadee6224786af894
gur90 <annettgur@gmail.com>
2022-07-03 17:10:27 +0300

    fix: fix url

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'd7d3184b18c44f229c1cedde435628ce', // получите свой ключ https://newsapi.org/
         });
     }
